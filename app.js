1838d237de23eed3dee265de37a2111756796ec9
Adamo <adammicahellayman@gmail.com>
2024-11-12 12:03:22 -0500

    added a win message,reset button, and a limit

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 let cookieCount = 0;
 
-const button = document.getElementById('cookie-button');
+const button = document.querySelector('#cookie-button');
 const cookieCountDisplay = document.getElementById('cookie-count');
 
 button.addEventListener('click', () => {
     cookieCount++;
     cookieCountDisplay.textContent = cookieCount;
+
+    if (cookieCount === 10) {
+        document.getElementById('winMessage').style.display = 'block';
+    }
 }); 
+
+let cookieRest = 0;
+
+const buttonElement = document.querySelector('#cookieReset');
+
+buttonElement.addEventListener('click', () => {
+    cookieCount = 0;
+    cookieCountDisplay.textContent = 0;
+    document.getElementById('winMessage').style.display = 'none';
+});
